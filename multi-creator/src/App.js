1dd41ee0904df197b392d86c5f808433d3d30427
Nitish Kumar <kumar.nit1021@gmail.com>
2023-10-17 21:40:10 +0530

    First Commit, Initialized React and tailwind

---
+++ multi-creator/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<div>
+				<h1 className="text-3xl text-blue-700">
+					Hello!!! Welcome to Multi creator..
+				</h1>
+			</div>
+		</div>
+	);
+}
+
+export default App;
